6efcf0d6eca03afb454dfb0d076bbeb74d7cfb6d
Christian Gunderman <cdg46@case.edu>
2014-12-15 00:13:12 -0500

    Added skeleton node class, header, and unittest file.

---
+++ library/src/node.cc
@@ -0,0 +1,14 @@
+// Gunderscript-2 Parse/AST Node
+// (C) 2014 Christian Gunderman
+
+#include "node.h"
+
+namespace gunderscript {
+namespace library {
+
+Node::Node() {
+
+}
+
+} // namespace library
+} // namespace gunderscript
---
+++ library/src/node.h
@@ -0,0 +1,19 @@
+// Gunderscript-2 Parse/AST Node
+// (C) 2014 Christian Gunderman
+
+#ifndef GUNDERSCRIPT_NODE__H__
+#define GUNDERSCRIPT_NODE__H__
+
+namespace gunderscript {
+namespace library {
+
+class Node {
+ public:
+  Node();
+
+};
+
+} // namespace library
+} // namespace gunderscript
+
+#endif // GUNDERSCRIPT_NODE__H__
